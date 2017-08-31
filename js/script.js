fbccf5ec95434908667547cd457b5415f494416c
Michael Edwards <edwardsmichaelv@gmail.com>
2017-08-31 17:30:17 -0400

    added side navigation bar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-// Apply lettering() to the name title
+  // Apply lettering() to the name title
   $("#name-title").lettering();
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop(),
+    docHeight = $('body').height(),
+    winHeight = $(window).height();
+    var totalScroll = (winTop/(docHeight-winHeight))*100;
+    $(".KW_progressBar").css("height",totalScroll+"%");
+  });
+  $(".side-nav").hover(
+    function() {
+      $(".side-nav-link").fadeIn(400);
+    },
+    function() {
+      $(".side-nav-link").fadeOut(400);
+    }
+  );
+  $(".demo-box").click(function() {
+
+  });
 });
--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
-// Apply lettering() to the name title
-  $("#name-title").lettering();
+
+$(window).load(function(){
+  console.log("i'm in");
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop(),
+    docHeight = $('article').height(),
+    winHeight = $(window).height();
+    console.log(winTop);
+    var totalScroll = (winTop/(docHeight-winHeight))*100;
+    console.log("total scroll" + totalScroll);
+    $(".KW_progressBar").css("width",totalScroll+"%");
+  });
+
 });
+// $(document).ready(function() {
+// // Apply lettering() to the name title
+//   $("#name-title").lettering();
+//   $(".demo-box").click(function() {
+//
+//   });
+// });
